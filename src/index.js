0b091b31a65083e0dd21b7e62594c6638e3c03c0
nicolasnazzaro <nicolas.nazzaro@gmail.com>
2020-06-11 19:29:34 +0200

    Routes fixed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
+  <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
     <App />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
