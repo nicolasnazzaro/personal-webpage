347121015105af212ac83f6f934ec9adcb39ebb0
nicolasnazzaro <nicolas.nazzaro@gmail.com>
2020-06-11 18:24:31 +0200

    Updates

---
+++ precache-manifest.25aaf5302537ec8119b6bf5970733517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fd3f9aa037b9bc97e07faf9e341fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "845056001b920fada886",
+    "url": "/static/css/main.404f7349.chunk.css"
+  },
+  {
+    "revision": "90a3605322044d79165b",
+    "url": "/static/js/2.14bd8ce7.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.14bd8ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845056001b920fada886",
+    "url": "/static/js/main.a77ce62f.chunk.js"
+  },
+  {
+    "revision": "352b85bc12efb46ed6db",
+    "url": "/static/js/runtime-main.3f1b9bfc.js"
+  }
+]);
