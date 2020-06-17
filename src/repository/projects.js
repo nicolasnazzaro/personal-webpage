1687e402964ca7279fe7e13bb2ec9e0e0f42c03f
nicolasnazzaro <nicolas.nazzaro@gmail.com>
2020-06-17 17:19:52 +0200

    Typos repository

--- src/repository/projects.js
+++ src/repository/projects.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         title: 'Reactivities',
         url: 'http://reactivities.nicolasnazzaro.me',
-        description: 'Twitter clone based on a .Net Core web api. The front-end is built with a react spa developed with typescript and MobX as a state management library. Registration and authentication are managed through .Net Core Identity. This application is designed for desktop experience only.',
+        description: 'Twitter clone based on a .NET Core webapi. The front-end is built with a react SPA developed with typescript and MobX for state management. Registration and authentication are managed through .NET Core Identity and jwt tokens. This application is designed for desktop experience only.',
         tags: ['react', 'react hooks', 'mobx', 'typescript', '.net core', 'core identity', 'webapi'],
         image: '/assets/reactivities.png',
         openSource: true,
@@ -11,7 +11,7 @@ export const projects = [
     {
         title: 'Kanban board',
         url: 'https://nicolasnazzaro.github.io/kanban-board/',
-        description: 'Trello inspired kanban board, realised with react-beatiful-dnd. Lists and cards are draggable and droppable. The board state is managed through Redux and the changes are saved in local storage.',
+        description: 'Trello inspired kanban board, realized with react-beatiful-dnd. Lists and cards are draggable and droppable. The board state is managed through Redux and the changes are saved in local storage.',
         tags: ['react', 'react hooks', 'redux'],
         image: '/assets/kanban.png',
         openSource: true,
@@ -29,7 +29,7 @@ export const projects = [
     {
         title: 'Simple To-Do app',
         url: 'https://nicolasnazzaro.github.io/to-do-app/',
-        description: 'Simple to do app create with react stateful components.',
+        description: 'Simple to-do app create with react stateful components.',
         tags: ['react'],
         image: '/assets/todo.png',
         openSource: true,
